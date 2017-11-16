0ec5623c2fd5e5b8c6b6a7ed45cf2fc9ccc59339
Luiz Gonçalves <luiz.goncalves@ufpr.br>
2017-11-16 21:03:33 -0200

    add basic operations

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -1,6 +1,29 @@
 (ns forca.core
   (:gen-class))
 
+(def totaldevidas 6)
+
+(defn perdeu [] (print "Voce perdeu"))
+(defn ganhou [] (print "Voce ganhou"))
+
+(defn letras-faltantes [palavra acertos]
+    (remove (fn [letra] (contains? acertos (str letra))) palavra )
+)
+
+(defn acertou-a-palavra-toda? [palavra acertos]
+    (empty? (letras-faltantes palavra acertos))
+)
+
+(defn jogo [vidas palavra acertos]
+   (if (= vidas 0)
+        (perdeu)
+        (if (acertou-a-palavra-toda? palavra acertos)
+            (ganhou)
+            (print "Chuta, amigo")
+        )
+    )
+)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
