091ba40de0960c26f5da0617ff943008a2c8da59
Luiz Gonçalves <luiz.goncalves@ufpr.br>
2017-11-16 21:44:10 -0200

    refact conditional sentences and add recur resource

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -3,8 +3,8 @@
 
 (def totaldevidas 6)
 
-(defn perdeu [] (print "Voce perdeu"))
-(defn ganhou [] (print "Voce ganhou"))
+(defn perdeu [] (println "Voce perdeu"))
+(defn ganhou [] (println "Voce ganhou"))
 
 (defn letras-faltantes [palavra acertos]
     (remove (fn [letra] (contains? acertos (str letra))) palavra )
@@ -20,21 +20,24 @@
     (.contains palavra chute)
 )
 
-(defn avalia-chute [chute vidas palavra acertos]
-    (if (acertou? chute palavra)
-        (jogo vidas palavra (conj acertos chute))
-        (jogo (dec vidas) palavra acertos)
-    )
-)
-
 (defn jogo [vidas palavra acertos]
-   (if (= vidas 0)
-        (perdeu)
-        (if (acertou-a-palavra-toda? palavra acertos)
-            (ganhou)
-            (avalia-chute (le-letra!) vidas palavra acertos)
+   (cond
+        (= vidas 0) (perdeu)
+        (acertou-a-palavra-toda? palavra acertos) (ganhou)
+        :else
+        (let [chute (le-letra!)]
+             (if (acertou? chute palavra)
+                (do
+                    (println "Acertou a letra!")
+                    (recur vidas palavra (conj acertos chute))
+                )
+                (do
+                    (println "Errou a letra!")
+                    (recur (dec vidas) palavra acertos)
+                )
+             )
         )
-    )
+   )
 )
 
 (defn -main
